412713704f8f8b98edf9841cfa94ccf7637b44b2
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-25 21:39:25 +0600

    method parser: implemented method for parsing arguments

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,6 +1,14 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MethodParser {
+
+    private static final Pattern METHOD_SIGNATURE_REGEX =
+            Pattern.compile("^((?<accessModifier>\\w+)\\s+)?(?<returnType>\\w+)\\s+(?<methodName>\\w+)\\s*\\((?<arguments>.*)\\)$");
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
@@ -20,6 +28,38 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Matcher matcher = METHOD_SIGNATURE_REGEX.matcher(signatureString.strip());
+
+        if (!matcher.matches()) {
+            throw new RuntimeException("Invalid method signature: " + signatureString);
+        }
+
+        String accessModifier = matcher.group("accessModifier");
+        String returnType = matcher.group("returnType");
+        String methodName = matcher.group("methodName");
+        String argumentsString = matcher.group("arguments");
+
+        return null;
     }
+
+    private List<MethodSignature.Argument> parseArguments(String argumentsString) {
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+        if (argumentsString.isBlank()) {
+            return arguments;
+        }
+
+        for (String argumentString : argumentsString.split("\\s*,\\s*")) {
+            String[] typeAndName = argumentString.split("\\s+");
+
+            if (typeAndName.length != 2) {
+                throw new IllegalArgumentException("Invalid argument: " + argumentString);
+            }
+
+            arguments.add(new MethodSignature.Argument(typeAndName[0], typeAndName[1]));
+        }
+
+        return arguments;
+    }
+
 }
